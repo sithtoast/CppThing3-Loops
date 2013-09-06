06e687f1844f7b664cd0c5f7a9b0bf8765a1dd0a
William Holt <william@toastedlabs.com>
2013-09-06 09:59:24 -0500

    First commit

---
+++ C++ Thing 3 - Loops/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  C++ Thing 3 - Loops
+//
+//  Created by William Holt on 9/6/13.
+//  Copyright (c) 2013 William Holt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World! Testing...\n";
+
+    return 0;
+}
