7be73939530dfcb081b2718dc098bb88e16b65e8
William Holt <william@toastedlabs.com>
2013-09-10 23:59:32 -0500

    First problem down. On to the second one!

--- C++ Thing 3 - Loops/main.cpp
+++ C++ Thing 3 - Loops/main.cpp
@@ -16,17 +16,33 @@ int main(int argc, const char * argv[])
 {
     double hours = 0.00;
     double rate = 0.00;
-    double drate = rate + (rate*0.5);
-
+    double drate;
+    double overtime;
+    double otsal;
+    double otbux;
     double bux;
 
-    cout << "Enter number of hours worked: ";
-    cin >> hours;
-    cout << "Enter hourly rate: ";
-    cin >> rate;
-    bux = hours * rate;
-    cout << "Your salary is: " << setprecision(2) << fixed << bux << endl;
+    while (hours != -1) {
+        cout << "Enter number of hours worked: (-1 to end) ";
+        cin >> hours;
+        cout << "Enter hourly rate: ($00.00) ";
+        cin >> rate;
+        if (hours >= 41) {
+            bux = 40 * rate;
+            drate = rate + (rate*0.5);
+            overtime = hours - 40;
+            otsal = drate * overtime;
+            otbux = otsal + bux;
+            cout << "Your salary is: " << setprecision(2) << fixed << otbux << endl;
+            cout << "You worked " << overtime << " hours overtime." << endl;
+            cout << "You made $" << otsal << " in overtime pay." << endl;
+            cout << "You made $" << bux << " in regular pay." << endl;
+        }
+        else {
+            bux = hours * rate;
+            cout << "Your salary is: " << setprecision(2) << fixed << bux << endl;
+        }
 
-
+    }
     return 0;
-}
+    }
